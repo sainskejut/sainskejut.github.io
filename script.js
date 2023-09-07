c2e8b06047bbbb72273ff32ec04c319bc72ce05d
sainskejut <144309764+sainskejut@users.noreply.github.com>
2023-09-07 14:04:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -79,4 +79,50 @@ function handleAnswer(index) {
     }
 }
 
+function handleAnswer(index) {
+    const answer = questions[currentQuestion].answers[index];
+    
+    // Record the answer
+    const recordDiv = document.createElement("div");
+    recordDiv.innerText = questions[currentQuestion].text + ": " + answer.text;
+    document.getElementById("answer-record").appendChild(recordDiv);
+    
+    if (answer.nextQuestion === -1) {
+        document.getElementById("result").innerText = answer.result;
+        document.getElementById("question-container").innerHTML = "";
+    } else {
+        currentQuestion = answer.nextQuestion;
+        displayQuestion();
+    }
+}
+
+document.getElementById("reset-btn").addEventListener("click", function() {
+    currentQuestion = 0;
+    document.getElementById("answer-record").innerHTML = "";
+    document.getElementById("result").innerText = "";
+    displayQuestion();
+});
+
+document.getElementById("email-btn").addEventListener("click", function() {
+    const subject = "Sales Problem Diagnosis Results";
+    const body = document.getElementById("answer-record").innerText + "\n\n" + document.getElementById("result").innerText;
+    window.location.href = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+});
+
+document.getElementById("print-btn").addEventListener("click", function() {
+    window.print();
+});
+
+document.getElementById("share-btn").addEventListener("click", function() {
+    // This is a basic example using the Web Share API (works on supported mobile browsers)
+    if (navigator.share) {
+        navigator.share({
+            title: 'Sales Problem Diagnosis Results',
+            text: document.getElementById("answer-record").innerText + "\n\n" + document.getElementById("result").innerText,
+            url: window.location.href,
+        });
+    } else {
+        alert("Sharing is not supported on this browser.");
+    }
+});
 displayQuestion();
