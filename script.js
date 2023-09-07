5f34c8faa84da61254342a76745f5220dd02581f
sainskejut <144309764+sainskejut@users.noreply.github.com>
2023-09-07 14:28:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const questions = [
     {
         text: "Have sales process?",
         answers: [
-            { text: "No", nextQuestion: -1, result: "No system in place" },
+            { text: "No", nextQuestion: -1, result: "Your PROBLEM is No system in place" },
             { text: "Yes", nextQuestion: 1 }
         ]
     },
     {
         text: "Getting over 300 opt in?",
         answers: [
-            { text: "No", nextQuestion: -1, result: "Not enough data, solution is to nail down offer and close, so testing has greater chance of success, then send more traffic." },
+            { text: "No", nextQuestion: -1, result: "Your PROBLEM is Not enough data, so your SOLUTION is to nail down offer and close, so testing has greater chance of success, then send more traffic." },
             { text: "Yes", nextQuestion: 2 }
         ]
     },
     {
         text: "Have more than 10 appointments?",
         answers: [
-            { text: "No", nextQuestion: -1, result: "No follow up" },
+            { text: "No", nextQuestion: -1, result: "Your PROBLEM is No follow up" },
             { text: "Yes", nextQuestion: 3 }
         ]
     },
@@ -26,35 +26,35 @@ const questions = [
         text: "Closing?",
         answers: [
             { text: "No", nextQuestion: 4 },
-            { text: "Yes", nextQuestion: -1, result: "You are the bottleneck. Solution = replace yourself and scale." }
+            { text: "Yes", nextQuestion: -1, result: "Your PROBLEM is You are the bottleneck. Solution = replace yourself and scale." }
         ]
     },
     {
         text: "Have more than 20 appointments?",
         answers: [
-            { text: "No", nextQuestion: -1, result: "No follow up" },
+            { text: "No", nextQuestion: -1, result: "Your PROBLEM is No follow up" },
             { text: "Yes", nextQuestion: 5 }
         ]
     },
     {
         text: "Money rejection?",
         answers: [
-            { text: "No", nextQuestion: -1, result: "Real problem = closing" },
+            { text: "No", nextQuestion: -1, result: "Your PROBLEM is you are the bottleneck or you got tired. SOLUTION is to recruit more salesperson to replace or replicate yourself, and scale up sales from here. Wow!" },
             { text: "Yes", nextQuestion: 6 }
         ]
     },
     {
         text: "Have sales?",
         answers: [
-            { text: "No", nextQuestion: -1, result: "Real problem = closing" },
+            { text: "No", nextQuestion: -1, result: "Your PROBLEM is closing. SOLUTION is to tweak your sales closing approach." },
             { text: "Yes", nextQuestion: 7 }
         ]
     },
     {
         text: "Low or high sales?",
         answers: [
-            { text: "Low", nextQuestion: -1, result: "Real problem = filter" },
-            { text: "High", nextQuestion: -1, result: "You are the bottleneck. Solution = replace yourself and scale." }
+            { text: "Low", nextQuestion: -1, result: "Your PROBLEM is filtering leads. SOLUTION is to tweak your lead filtering so you get better qualified leads." },
+            { text: "High", nextQuestion: -1, result: "Your PROBLEM is you are the bottleneck or you got tired. SOLUTION is to recruit more salesperson to replace or replicate yourself, and scale up sales from here. Wow!" }
         ]
     }
 ];
